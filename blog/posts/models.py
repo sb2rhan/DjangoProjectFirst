82efa45154acf6eb4687fe39e6bd82b0b09c3204
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-11-16 17:57:50 +0600

    changes in model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-# Create your models here.
+# from django.contrib.auth.models import User # defaut Django user
+from django.contrib.auth import get_user_model # function to get default Django user
+from django.utils import timezone
 """
     Command that saves model in db by shell:
     > python manage.py shell
@@ -23,9 +24,31 @@ from django.db import models
     >> post = Post.objects.get(pk=1)
     >> post.title = 'another'
     >> post.save()
+
+    See user's posts:
+    >> from django.contrib.auth.models import User
+    >> u = User.objects.get(pk=1)
+    >> u.post_set.all()
 """
 
 class Post(models.Model):
+    STATUS = [
+        (1, 'published'),
+        (0, 'unpublished')
+    ]
+
     title = models.CharField(verbose_name='Название', max_length=120) # verbose_name is name in DB
     description = models.TextField(verbose_name='Описание', blank=True, default=None)
-    # publish_date = models.DateTimeField(verbose_name='Дата публикации', null=True)
+    publish_date = models.DateTimeField(verbose_name='Дата публикации', auto_now=timezone.now())
+    status = models.CharField(verbose_name='Статус', max_length=190, choices=STATUS, default=1)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=1) # model with this field is Many in relation Many to 1
+    # CASCADE means posts will be deleted when user is deleted from DB
+
+    tag = models.ManyToManyField('Tag') # now it does not show error and it is correct way
+    # tag is gonna be QuerySet
+    # Adding Tag to Post: >> post1.tag.add(tag1)
+
+class Tag(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=50)
+    slug = models.SlugField(verbose_name='Тематика', max_length=255, db_index=True)
+    # slug is a link, db_index=True makes this field as index
